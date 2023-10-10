a6d82e541c7ed4547c98a500ab82970b7a7cb164
arnaud <linder.arnaud@orange.fr>
2023-10-10 06:11:17 +0200

    easy admin

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use App\Repository\ImageClefRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccueilController extends AbstractController
+{
+    //page principal du site
+// route de la page ;: comment accèdé a la page depuis l'url 
+    #[Route('/accueil', name: 'app_accueil')]
+    public function index( ArticleRepository $articleRepository,ImageClefRepository $imageClefRepository): Response
+
+    {
+
+        $photos = $imageClefRepository->findBy([], ["id" => "ASC"]);
+        $articles = $articleRepository->findBy([], ["id" => "ASC"]);
+        return $this->render('accueil/index.html.twig', [
+            'articles' => $articles,
+            'photos' => $photos
+
+        ]);
+    }
+
+}
